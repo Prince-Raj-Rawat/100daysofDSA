8b9efaf8c65021324bd493ac08351e48e3c027a1
Prince Raj Rawat <prince1234rawat4321@gmail.com>
2026-02-01 15:53:02 +0530

    day1

---
+++ day1/q1.c
@@ -0,0 +1,48 @@
+/*Insert an Element in an Array
+
+Problem: Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers (the array)
+- Third line: integer pos (1-based position)
+- Fourth line: integer x (element to insert)
+
+Output:
+- Print the updated array (n+1 integers) in a single line, space-separated
+
+Example:
+Input:
+5
+1 2 4 5 6
+3
+3
+
+Output:
+1 2 3 4 5 6
+
+Explanation: Insert 3 at position 3, elements [4,5,6] shift right
+*/ 
+
+#include <stdio.h>
+
+int main(){ 
+    int n = 3;
+    int a[n+1];
+    for(int i=0;i<3;i++){
+        scanf("%d",&a[i]);
+    }
+    int b ;
+    scanf("%d",&b);
+    int pos;
+    scanf("%d",&pos);
+    for (int i= n;i >= pos-1; i --)
+    {
+        a[i]=a[i-1];
+    }
+    a[pos-1]=b;
+    for(int i=0;i<=3;i++){
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
