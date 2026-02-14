61e167e9cdb5992033f12b8a5a2ec55a721b2f4d
Prince Raj Rawat <prince1234rawat4321@gmail.com>
2026-02-14 12:28:27 +0530

    day7

--- day006/q6.c
+++ day006/q6.c
@@ -1,3 +1,21 @@
+/*Problem: Given a sorted array of n integers, remove duplicates in-place. Print only unique elements in order.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers (sorted array)
+
+Output:
+- Print unique elements only, space-separated
+
+Example:
+Input:
+6
+1 1 2 2 3 3
+
+Output:
+1 2 3
+
+Explanation: Keep first occurrence of each element: 1, 2, 3*/
 #include <stdio.h>
 void bubble_sort(int *ptr,int *size);
 void remove_duplicate(int *ptr,int *size);
